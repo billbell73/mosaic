4f032b4fbe93e436dcd5686e0dee7ba4f71408dd
Will Bell <willbell@gmail.com>
2016-04-16 21:05:33 +0100

    time different bits of functionality

--- cmd/mosaicServer/controller.go
+++ cmd/mosaicServer/controller.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	_ "image/png"
 
@@ -24,6 +25,7 @@ func newHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func showHandler(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
 	r.ParseMultipartForm(10485760) // max body in memory is 10MB
 	file, _, err := r.FormFile("image")
 	log.Printf("%T", file)
@@ -33,6 +35,8 @@ func showHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	t05 := time.Now()
+
 	original, format, err := image.Decode(file)
 	if err != nil {
 		log.Fatal(err)
@@ -40,13 +44,24 @@ func showHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Format: ", format)
 	log.Println("Bounds of original image: ", original.Bounds())
 
+	t1 := time.Now()
+
 	mosaic, width := createMosaic(original)
+
+	t2 := time.Now()
 
 	buffer := new(bytes.Buffer)
 	if err := jpeg.Encode(buffer, original, nil); err != nil {
 		log.Fatal("Unable to encode original image: ", err)
 	}
 	str := base64.StdEncoding.EncodeToString(buffer.Bytes())
+
+	t3 := time.Now()
+
+	log.Println("Parse form:", t05.Sub(t0))
+	log.Println("decode file:", t1.Sub(t05))
+	log.Println("Create mosaic:", t2.Sub(t1))
+	log.Println("Encode original:", t3.Sub(t2))
 
 	t, err := template.ParseFiles("views/show.html")
 	if err != nil {
